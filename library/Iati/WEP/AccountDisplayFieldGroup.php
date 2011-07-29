7bfaa2a2e50ecaf532cfb2e7238fbddb55975e3a
manisha <manisha@manisha-laptop.(none)>
2011-07-29 17:42:48 +0545

    changes in default field groups

--- library/Iati/WEP/AccountDisplayFieldGroup.php
+++ library/Iati/WEP/AccountDisplayFieldGroup.php
@@ -3,9 +3,10 @@ class Iati_WEP_AccountDisplayFieldGroup
 {
     protected $title = '0';
     protected $activity_date = '0';
-    protected $participating_organisation = '0';
+    protected $participating_org = '0';
     protected $transaction = '0';
     protected $default_tied_status = '0';
+    protected $transaction = '0';
     
     public function getProperties(){
         return get_object_vars($this);
