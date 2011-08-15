1ce2356619eda58b4af220c4a5cd6dae5a2a57df
manisha <manisha@manisha-laptop.(none)>
2011-08-15 17:40:59 +0545

    javascript

--- public/js/common.js
+++ public/js/common.js
@@ -205,17 +205,17 @@ function initialize() {
         },
         "tr": {
             "onmouseenter" : function (evt) {
-        		console.log(evt);
+//        		console.log(evt);
         		var node = dojo.NodeList(evt.target.parentNode);
-        		console.log(node);
+//        		console.log(node);
         		node.query('.list-action').style('display', 'block');;
         
         		evt.stopPropagation();
         	},
         	"onmouseleave" : function (evt) {
-        		console.log(evt);
+//        		console.log(evt);
         		var node = dojo.NodeList(evt.target.parentNode);
-        		console.log(node);
+//        		console.log(node);
         		node.query('.list-action').style('display', 'none');;
         
         		evt.stopPropagation();
