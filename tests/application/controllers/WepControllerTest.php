7f802c32f077f367edc4eb80b504a464389c7589
manisha <manisha@manisha-laptop.(none)>
2011-08-16 10:28:27 +0545

    wep controlle added

---
+++ tests/application/controllers/WepControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+class WepControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+    public function setUp()
+    {
+        $this->bootstrap = array($this, 'appBootstrap');
+        parent::setUp();
+    }
+    
+    public function appBootstrap()
+    {
+        $this->_application = new Zend_Application(APPLICATION_ENV,
+              APPLICATION_PATH . '/configs/application.ini'
+        );
+        $this->_application->bootstrap();
+        
+        /**
+         * Fix for ZF-8193
+         * http://framework.zend.com/issues/browse/ZF-8193
+         * Zend_Controller_Action->getInvokeArg('bootstrap') doesn't work
+         * under the unit testing environment.
+         */
+        $front = Zend_Controller_Front::getInstance();
+        if($front->getParam('bootstrap') === null) {
+            $front->setParam('bootstrap', $this->_application->getBootstrap());
+        }
+    }
+    
+
+    public function tearDown()
+    {
+        /* Tear Down Routine */
+    }
+    
+    public function testDashboard()
+    {
+
+    }
+    
+}
