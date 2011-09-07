f1d55422319ef1a79153ef2b29e4aac20dcaec06
manisha <manisha@manisha-laptop.(none)>
2011-09-07 11:36:49 +0545

    changes in permission constatn class

--- library/Iati/WEP/PermissionConts.php
+++ library/Iati/WEP/PermissionConts.php
@@ -4,13 +4,13 @@ class Iati_WEP_PermissionConts
     private function __construct() {
 
     }
-    static $ADD_ACTIVITY = 'add_activity';
-    static $EDIT_ACTIVITY = 'edit_activity';
-    static $DELETE_ACTIVITY = 'delete_activity';
-    static $VIEW_ACTIVITIES = 'view_activities';
-    static $ADD_ACTIVITY_ELEMENTS = 'add_activity_elements';
-    static $EDIT_ACTIVITY_ELEMENTS = 'edit_activity_elements';
-    static $DELETE_ACTIVITY_ELEMENTS = 'delete_activity_elements';
-    //static $EDIT_DEFAULTS = 'edit_defaults';
-    static $PUBLISH = 'publish';
+    const ADD_ACTIVITY = 'add_activity';
+    const EDIT_ACTIVITY = 'edit_activity';
+    const DELETE_ACTIVITY = 'delete_activity';
+    const VIEW_ACTIVITIES = 'view_activities';
+    const ADD_ACTIVITY_ELEMENTS = 'add_activity_elements';
+    const EDIT_ACTIVITY_ELEMENTS = 'edit_activity_elements';
+    const DELETE_ACTIVITY_ELEMENTS = 'delete_activity_elements';
+    //static EDIT_DEFAULTS = 'edit_defaults';
+    const PUBLISH = 'publish';
 }
