2520ea81b46414ef5897e1ac22b7e04ac2483126
manisha <manisha@manisha-laptop.(none)>
2011-09-09 14:35:18 +0545

    bugs fixed

--- library/Iati/Activity/Element/DocumentLink.php
+++ library/Iati/Activity/Element/DocumentLink.php
@@ -3,4 +3,5 @@ class Iati_Activity_Element_DocumentLink extends Iati_Activity_Element
 {
     protected $_type = 'DocumentLink';
     protected $_parentType = 'Activity';
+    protected $_validAttribs = array('@url' => '');
 }
