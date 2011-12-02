a6d85c5927ef3d46fa8575f4027bf0dc1250db73
Development Server <dev@localserver.yipl.com.np>
2011-12-02 15:15:40 +0545

    scroll for index page

--- public/js/scroll-content.js
+++ public/js/scroll-content.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
 	$(".nav a").click(function() {
+		//removeActive();
+		//$(this).addClass('active');
 		var id = $(this).attr('href');
-		$('html,body').stop().animate({scrollTop: $(id).offset().top}, 1000);
+		$('html,body').stop().animate({scrollTop: $(id).offset().top}, 300,'swing');
 		return false;
 	});
+
+	var what = $('#What-is-AidStream').height();
+	var how = what + $('#How-to-use-AidStream').height();
+
+	$(document).scroll(function() {
+        if(($(this).scrollTop() >= what) && ($(this).scrollTop() < how)) {
+			removeActive();
+           $("#how-link").addClass('active');
+        }
+	else if($(this).scrollTop() >= how) {
+		removeActive();
+           $("#contact-link").addClass('active');
+        }
+	else{
+		removeActive();
+           $("#what-link").addClass('active');
+	}
+    });
 })
+
+function removeActive()
+{
+		$(".nav li").each(function(index, element) {
+			$(element).children('a').removeClass('active');
+		});
+}
