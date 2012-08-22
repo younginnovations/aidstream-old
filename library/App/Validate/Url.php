f8ac4381efe512152b9b61fccb3da1443b489a37
bhabishyat <bhabishyat@gmail.com>
2012-08-22 15:15:35 +0545

    newline check added to url validate and message changed

--- library/App/Validate/Url.php
+++ library/App/Validate/Url.php
@@ -4,11 +4,16 @@ class App_Validate_Url extends Zend_Validate_Abstract
     const MSG_URL = 'msgUrl';
 
     protected $_messageTemplates = array(
-        self::MSG_URL => "Invalid URL , Please enter url in format http://mysite.com",
+        self::MSG_URL => "Invalid URL , Please enter a single url in valid url format e.g http://mysite.com",
     );
 
     public function isValid($value)
     {
+        if(preg_match('/\n/' , $value)){
+            $this->_error(self::MSG_URL);
+            return false;
+        }
+        
         if (preg_match('"(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"', $value))  {
             return true;
         } else {
