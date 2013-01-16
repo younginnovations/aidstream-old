bbeff0448f17730c52f437b836670ac068b48630
Sita Prajapati <sita prajapati>
2013-01-16 14:50:24 +0545

    ReportinOrg and Identifier was added as childelements

--- library/Iati/Aidstream/Element/Organisation.php
+++ library/Iati/Aidstream/Element/Organisation.php
@@ -6,7 +6,7 @@ class Iati_Aidstream_Element_Organisation extends Iati_Core_BaseElement
     protected $className = 'Organisation';
     protected $displayName = 'Organisation';
     protected $tableName = 'iati_organisation';
-    protected $childElements = array('Name' , 'TotalBudget' , 'RecipientOrgBudget' , 'RecipientCountryBudget' , 'DocumentLink');
+    protected $childElements = array('ReportingOrg','Identifier','Name' , 'TotalBudget' , 'RecipientOrgBudget' , 'RecipientCountryBudget' , 'DocumentLink');
     protected $attribs = array('id' , '@xml_lang' , '@default_currency' , '@last_updated_datetime' , 'account_id');
     protected $iatiAttribs = array('@xml_lang' , '@default_currency' , '@last_updated_datetime');
     
