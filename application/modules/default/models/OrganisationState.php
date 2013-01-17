774c635955bf357818c12c06a55d15cfa891ae89
Sita Prajapati <sita prajapati>
2013-01-17 23:20:10 +0545

    variable name was corrected

--- application/modules/default/models/OrganisationState.php
+++ application/modules/default/models/OrganisationState.php
@@ -3,8 +3,8 @@ class Model_OrganisationState extends Zend_Db_Table_Abstract
 {
     protected $_name = 'iati_organisation';
     
-    public function updateOrganisationState($organisations_id,$state_id)
+    public function updateOrganisationState($organisationIds,$stateId)
     {
-        parent::update(array('state_id'=>$state_id),array('id IN(?)'=>$organisations_id));
+        parent::update(array('state_id'=>$stateId),array('id IN(?)'=>$organisationIds));
     }
 }
