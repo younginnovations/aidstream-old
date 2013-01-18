ef00f04b064fd5dfb95343ec2859a483d0ec68c4
Sita Prajapati <sita prajapati>
2013-01-18 22:53:56 +0545

    form was changed

--- library/Iati/Aidstream/Form/Organisation/RecipientOrgBudget/RecipientOrg.php
+++ library/Iati/Aidstream/Form/Organisation/RecipientOrgBudget/RecipientOrg.php
@@ -15,7 +15,7 @@ class Iati_Aidstream_Form_Organisation_RecipientOrgBudget_RecipientOrg extends I
         $form['id'] = new Zend_Form_Element_Hidden('id');
         $form['id']->setValue($this->data['id']);
         
-        $form['ref'] = new Zend_Form_Element_Text('code');
+        $form['ref'] = new Zend_Form_Element_Text('ref');
         $form['ref']->setLabel('Ref')
             ->setValue($this->data['@ref'])
             ->setRequired()
