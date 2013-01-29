a7bea0b7873bb29849a49edb4aafb3321f3585c8
Sita Prajapati <sita prajapati>
2013-01-29 16:01:58 +0545

    result file was created

---
+++ library/Iati/Aidstream/Element/Activity/Result.php
@@ -0,0 +1,18 @@
+<?php
+
+class Iati_Aidstream_Element_Activity_Result extends Iati_Core_BaseElement
+{
+    protected $isMultiple = true;
+    protected $className = 'Result';
+    protected $displayName = 'Result';
+    protected $isRequired = true;
+    protected $tableName = 'iati_result';
+    protected $attribs = array('id' , '@type' , '@aggregation_status');
+    protected $iatiAttribs = array('@type' , '@aggregation_status');
+    protected $childElements = array(
+                                     'Title' , 
+                                     'Description' ,
+                                     'Indicator'
+                               );
+    
+}
