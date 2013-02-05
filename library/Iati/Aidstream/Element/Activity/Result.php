1adc088da3e8c071da7b80e21fe89b9537fe2151
Sita Prajapati <sita prajapati>
2013-02-05 15:34:05 +0545

    multiple result was removed

--- library/Iati/Aidstream/Element/Activity/Result.php
+++ library/Iati/Aidstream/Element/Activity/Result.php
@@ -2,7 +2,6 @@
 
 class Iati_Aidstream_Element_Activity_Result extends Iati_Core_BaseElement
 {
-    protected $isMultiple = true;
     protected $className = 'Result';
     protected $displayName = 'Result';
     protected $isRequired = true;
