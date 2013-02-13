58661f2b4ee62af70ab599f37f1d7b685aa03c3b
Sita Prajapati <sita prajapati>
2013-02-13 12:06:11 +0545

    required was removed

--- library/Iati/Aidstream/Element/Activity/Result.php
+++ library/Iati/Aidstream/Element/Activity/Result.php
@@ -5,7 +5,6 @@ class Iati_Aidstream_Element_Activity_Result extends Iati_Core_BaseElement
     protected $isMultiple = true;
     protected $className = 'Result';
     protected $displayName = 'Result';
-    protected $isRequired = true;
     protected $tableName = 'iati_result';
     protected $attribs = array('id' , '@type' , '@aggregation_status');
     protected $iatiAttribs = array('@type' , '@aggregation_status');
