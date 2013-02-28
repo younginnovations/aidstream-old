44204312ff1236fca131e64d8786c999b8c4fe38
Sita Prajapati <sita prajapati>
2013-02-28 11:40:47 +0545

    help link was removed which was overwrite

--- library/Iati/Aidstream/Form/Organisation/ReportingOrg.php
+++ library/Iati/Aidstream/Form/Organisation/ReportingOrg.php
@@ -16,32 +16,28 @@ class Iati_Aidstream_Form_Organisation_ReportingOrg extends Iati_Core_BaseForm
         $form['@ref']->setLabel('Identifier')
                 ->setValue($this->data['@ref'])
                 ->setRequired()
-                ->setAttribs(array('disabled' => 'disabled'))
-                ->addDecorators(array(array('HtmlTag' , array('tag' => 'div' , 'class' => 'help Organisation_ReportingOrg-ref' , 'placement' => 'PREPEND'))));
+                ->setAttribs(array('disabled' => 'disabled'));
         
         $reportingOrgType = $model->getRowsByFields('OrganisationType','id', $this->data['@type']);
         $form['type'] = new Zend_Form_Element_Select('type');
         $form['type']->setLabel('Type')
                 ->setAttrib('class' , 'form-select')
                 ->addMultiOptions(array($this->data['@type']=>$reportingOrgType[0]['Name']))
-                ->setAttribs(array('disabled' => 'disabled'))
-                ->addDecorators(array(array('HtmlTag' , array('tag' => 'div' , 'class' => 'help Organisation_ReportingOrg-type' , 'placement' => 'PREPEND'))));
+                ->setAttribs(array('disabled' => 'disabled'));
 
         $form['text'] = new Zend_Form_Element_Textarea('text');
         $form['text']->setLabel('Name')
                 ->setValue($this->data['text'])
                 ->setAttrib('cols' , '40')
                 ->setAttrib('rows' , '2')
-                ->setAttribs(array('disabled' => 'disabled'))
-                ->addDecorators(array(array('HtmlTag' , array('tag' => 'div' , 'class' => 'help Organisation_ReportingOrg-text' , 'placement' => 'PREPEND'))));
+                ->setAttribs(array('disabled' => 'disabled'));
 
         $lang = $model->getRowsByFields('Language','id', $this->data['@xml_lang']);
         $form['xml_lang'] = new Zend_Form_Element_Select('@xml_lang');
         $form['xml_lang']->setLabel('Language')
                 ->setAttrib('class' , 'form-select')
                 ->setAttribs(array('disabled' => 'disabled'))
-                ->addMultioptions(array($this->data['@xml_lang']=>$lang[0]['Code'].'-'.$lang[0]['Name']))
-                ->addDecorators(array(array('HtmlTag' , array('tag' => 'div' , 'class' => 'help Organisation_ReportingOrg-xml_lang' , 'placement' => 'PREPEND'))));
+                ->addMultioptions(array($this->data['@xml_lang']=>$lang[0]['Code'].'-'.$lang[0]['Name']));
 
         $this->addElements($form);
         $this->setAction($baseurl . '/organisation/update-default/?elementName=ReportingOrg');
