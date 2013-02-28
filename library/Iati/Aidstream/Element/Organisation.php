4ba09874133e6b053f5ac19a2d5c45ec634aa06a
Sita Prajapati <sita prajapati>
2013-02-28 11:39:22 +0545

    default form was done

--- library/Iati/Aidstream/Element/Organisation.php
+++ library/Iati/Aidstream/Element/Organisation.php
@@ -2,12 +2,57 @@
 
 class Iati_Aidstream_Element_Organisation extends Iati_Core_BaseElement
 {
+
     protected $isMultiple = false;
     protected $className = 'Organisation';
-    protected $displayName = 'Organisation';
+    protected $displayName = 'Organisation Default';
     protected $tableName = 'iati_organisation';
-    protected $childElements = array('ReportingOrg','Identifier','Name' , 'TotalBudget' , 'RecipientOrgBudget' , 'RecipientCountryBudget' , 'DocumentLink');
+    protected $childElements = array('ReportingOrg' , 'Identifier' , 'Name' , 'TotalBudget' , 'RecipientOrgBudget' , 'RecipientCountryBudget' , 'DocumentLink');
     protected $attribs = array('id' , '@xml_lang' , '@default_currency' , '@last_updated_datetime' , 'account_id');
     protected $iatiAttribs = array('@xml_lang' , '@default_currency' , '@last_updated_datetime');
+
+    public function getForm($ajax = false)
+    {
+        $formname = preg_replace('/Element/' , 'Form' , get_class($this));
+        if ($this->data)
+        {
+            $eleForm = new $formname(array('element' => $this));
+            $form = $eleForm->getForm();
+            $form->prepare();
+        } else
+        {
+            $eleForm = new $formname(array('element' => $this));
+            if ($this->count)
+            {
+                $eleForm->setCount($this->count);
+            }
+            $form = $eleForm->getFormDefination();
+            $form->prepare();
+        }
+        $form->wrapForm($this->getDisplayName() , $this->getIsRequired());
+        return $form;
+    }
     
-}
+     public function save($data , $parentId = null)
+    { 
+       
+            $elementsData = $this->getElementsData($data);
+            $identity = Zend_Auth::getInstance()->getIdentity();
+            $elementsData['account_id'] = $identity->account_id;
+            
+            if($this->hasData($elementsData) ){
+                // If no id is present, insert the data else update the data using the id.
+                if(!$elementsData['id']){
+                    $elementsData['id'] = null;
+                    $eleId = $this->db->insert($elementsData);
+                } else {
+                    $eleId = $elementsData['id'];
+                    unset($elementsData['id']);
+                    $this->db->update($elementsData , array('id = ?' => $eleId));
+                }
+            }
+       
+        return $eleId;
+    }
+    
+}// Ends class
--- library/Iati/Aidstream/Form/Organisation.php
+++ library/Iati/Aidstream/Form/Organisation.php
@@ -12,25 +12,34 @@ class Iati_Aidstream_Form_Organisation extends Iati_Core_BaseForm
         $form['id'] = new Zend_Form_Element_Hidden('id');
         $form['id']->setValue($this->data['id']);
         
-        $form['account_id'] = new Zend_Form_Element_Hidden('account_id');
-        $form['account_id']->setValue($identity->account_id);
-        
-        $form['last_updated_datetime'] = new Zend_Form_Element_Hidden('last_updated_datetime');
-        $form['last_updated_datetime']->setValue(($this->data['@last_updated_datetime'])?$this->data['@last_updated_datetime']:date('Y-m-d h:i:s'));
-                
         $lang = $model->getCodeArray('Language', null, '1' , true);
         $form['xml_lang'] = new Zend_Form_Element_Select('xml_lang');
         $form['xml_lang']->setLabel('Language')
             ->setValue($this->data['@xml_lang'])
             ->setAttrib('class' , 'form-select')
+            ->setRequired()    
             ->addMultioptions($lang);
-            
-        $form['default_currency'] = new Zend_Form_Element_Text('default_currency');
-        $form['default_currency']->setLabel('Default Currency')
+        
+        $currency = $model->getCodeArray('Currency', null, '1' , true);
+        $form['default_currency'] = new Zend_Form_Element_Select('default_currency');
+        $form['default_currency']->setLabel('Currency')
             ->setValue($this->data['@default_currency'])
-            ->setAttribs(array('class' => 'form-text'));
+            ->setRequired()    
+            ->setAttrib('class' , 'form-select')
+            ->setMultioptions($currency);
 
         $this->addElements($form);
         return $this;
+    }
+    
+    public function addSubmitButton($label , $saveAndViewlabel = 'Save and View')
+    {
+        $this->addElement('submit' , 'update' , array(
+            'label' => 'update' ,
+            'required' => false ,
+            'ignore' => false ,
+                )
+        );
+
     }
 }
