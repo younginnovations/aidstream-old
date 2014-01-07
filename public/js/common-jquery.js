eba7c034c4008932ebb9b84273c222e73e195066
Saugat Acharya <saugat.acharya@yipl.com.np>
2014-01-07 15:17:35 +0545

    Removed settings.js - moved it's content to common-jquery.js

--- public/js/common-jquery.js
+++ public/js/common-jquery.js
@@ -10,4 +10,24 @@ $(document).ready(function(){
         evt.preventDefault();
         $('#login-form-wrapper').toggle();
     });
+
+    // Reporting Org Change Detection
+    var oldData = $("#fieldset-reporting_org_info :input[value!='']").serialize();
+    if (oldData != '') {
+        $('.defaults-forms form').one('submit', function(event){
+            var newData = $('#fieldset-reporting_org_info :input').serialize();
+            if (oldData != newData) {
+                event.preventDefault();
+                var confirmDialog = confirm('It seems you have changed your Reporting Org Information. Are you sure you want to continue?');
+                if (confirmDialog == true) {
+                    $(this).attr("action", "settings?btn=ok") 
+                    $(this).submit();
+                } else { 
+                    $(this).submit();
+                }
+            } else {
+                $(this).submit();
+            } 
+        });
+    }
 })
