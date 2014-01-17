f62e637fde280541def90014f3e4069bfc9c3b0e
Saugat Acharya <saugat.acharya@yipl.com.np>
2014-01-17 11:07:29 +0545

    Improper Indentation Resolved.

--- library/App/Validate/PasswordConfirmation.php
+++ library/App/Validate/PasswordConfirmation.php
@@ -1,27 +1,26 @@
 <?php
-class App_Validate_PasswordConfirmation extends Zend_Validate_Abstract {
+class App_Validate_PasswordConfirmation extends Zend_Validate_Abstract 
+{
 
-const NOT_MATCH = 'notMatch';
+	const NOT_MATCH = 'notMatch';
 
-protected $_messageTemplates = array(
-self::NOT_MATCH => 'Password confirmation does not match'
-);
+	protected $_messageTemplates = array(
+		self::NOT_MATCH => 'Password confirmation does not match'
+	);
 
-public function isValid($value, $context = null) {
-$value = (string) $value;
-$this->_setValue($value);
+	public function isValid($value, $context = null) {
+		$value = (string) $value;
+		$this->_setValue($value);
 
-if (is_array($context)) {
-if (
-isset($context['confirmpassword']) &&
-($value == $context['confirmpassword'])) {
-return true;
-}
-} elseif (is_string($context) && ($value == $context)) {
-return true;
-}
+		if (is_array($context)) {
+			if (isset($context['confirmpassword']) && ($value == $context['confirmpassword'])) {
+				return true;
+			}
+		} elseif (is_string($context) && ($value == $context)) {
+			return true;
+		}
 
-$this->_error(self::NOT_MATCH);
-return false;
-}
+		$this->_error(self::NOT_MATCH);
+		return false;
+	}
 }
