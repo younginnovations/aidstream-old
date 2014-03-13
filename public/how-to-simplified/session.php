39d39408606f814b1018b64d8581af91a8ad85fc
Saugat Acharya <saugat.acharya@yipl.com.np>
2014-03-13 14:00:37 +0545

    Stop tracking public/how-to and public/how-to-simplified

--- public/how-to-simplified/session.php
+++
@@ -1,9 +0,0 @@
-<?
- //use same session from aidstream;
-    DEFINE('SESSION_PATH' , dirname(__FILE__)."/../../data/session");
-
-    //ini_set('error_reporting' , E_ALL);
-    ini_set('session.save_path' ,SESSION_PATH);
-    session_name('IATI');
-    session_start();
-?>
