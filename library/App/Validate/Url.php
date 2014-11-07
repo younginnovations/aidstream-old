71eaf682ff97e52ea32a8cd68e1f047eae5421c6
Saugat Acharya <mesaugat@gmail.com>
2014-11-07 12:05:49 +0545

    Encode document links uploaded to AidStream.

--- library/App/Validate/Url.php
+++ library/App/Validate/Url.php
@@ -4,7 +4,7 @@ class App_Validate_Url extends Zend_Validate_Abstract
     const MSG_URL = 'msgUrl';
 
     protected $_messageTemplates = array(
-        self::MSG_URL => "Invalid URL , Please enter a single url in valid url format e.g http(s)://mysite.com",
+        self::MSG_URL => "Please enter a single url in valid url format with valid characters (spaces are not allowed) e.g http(s)://mysite.com/filename.pdf",
     );
 
     public function isValid($value)
