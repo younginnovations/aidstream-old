e1998c9c20e43a8f698b25a48660eaf6b975e5f7
Saugat Acharya <mesaugat@gmail.com>
2014-12-15 11:13:34 +0545

    Add Narrative child element for iati-activity/title.

--- library/Iati/Aidstream/Element/Activity/Title.php
+++ library/Iati/Aidstream/Element/Activity/Title.php
@@ -3,10 +3,10 @@
 class Iati_Aidstream_Element_Activity_Title extends Iati_Core_BaseElement
 {  
     protected $isMultiple = true;
+    protected $isRequired = true;
     protected $className = 'Title';
     protected $displayName = 'Title';
-    protected $isRequired = true;
     protected $tableName = 'iati_title';
-    protected $attribs = array('id','text','@xml_lang');
-    protected $iatiAttribs = array('text','@xml_lang');
+    protected $attribs = array('id');
+    protected $childElements = array('Narrative');
 }
