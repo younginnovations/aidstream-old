616b02cf1402fad00ee61c7cbf5f8f5507aca5d3
Saugat Acharya <mesaugat@gmail.com>
2014-12-15 22:50:42 +0545

    Remove @xml_lang and text fields for iati-activity/title.

--- library/Iati/Aidstream/Element/Activity/Title.php
+++ library/Iati/Aidstream/Element/Activity/Title.php
@@ -3,7 +3,6 @@
 class Iati_Aidstream_Element_Activity_Title extends Iati_Core_BaseElement
 {  
     protected $isMultiple = true;
-    protected $isRequired = true;
     protected $className = 'Title';
     protected $displayName = 'Title';
     protected $tableName = 'iati_title';
