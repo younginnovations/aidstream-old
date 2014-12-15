60672c1b063814b1a51ca7363ee9a8e588f2af00
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-15 10:55:29 +0545

    Narrative child element for country-budget-items/budget-item/description.

---
+++ library/Iati/Aidstream/Element/Activity/CountryBudgetItems/BudgetItem/Description/Narrative.php
@@ -0,0 +1,12 @@
+<?php
+class Iati_Aidstream_Element_Activity_CountryBudgetItems_BudgetItem_Description_Narrative extends Iati_Core_BaseElement
+{
+    protected $isMultiple = true;
+    protected $isRequired = true;
+    protected $className = 'Narrative';
+    protected $displayName = 'Narrative';
+    protected $tableName = 'iati_country_budget_items/budget_item/description/narrative';
+    protected $attribs = array('id', '@xml_lang', 'text');
+    protected $iatiAttribs = array('@xml_lang', 'text');
+    protected $viewScriptEnabled = true;
+}
