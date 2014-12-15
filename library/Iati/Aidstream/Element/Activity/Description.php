e68e983db21a9509c8ffce438248add0ebb500e0
Saugat Acharya <mesaugat@gmail.com>
2014-12-15 20:54:19 +0545

    Add Narrative child element for iati-activity/description.

--- library/Iati/Aidstream/Element/Activity/Description.php
+++ library/Iati/Aidstream/Element/Activity/Description.php
@@ -6,6 +6,7 @@ class Iati_Aidstream_Element_Activity_Description extends Iati_Core_BaseElement
     protected $className = 'Description';
     protected $displayName = 'Description';
     protected $tableName = 'iati_description';
-    protected $attribs = array('id', '@type', '@xml_lang', 'text');
-    protected $iatiAttribs = array('@type', '@xml_lang', 'text');
+    protected $attribs = array('id', '@type');
+    protected $iatiAttribs = array('@type');
+    protected $childElements = array('Narrative');
 }
--- library/Iati/Aidstream/Form/Activity/Description.php
+++ library/Iati/Aidstream/Form/Activity/Description.php
@@ -11,13 +11,6 @@ class Iati_Aidstream_Form_Activity_Description extends Iati_Core_BaseForm
         $form['id'] = new Zend_Form_Element_Hidden('id');
         $form['id']->setValue($this->data['id']);
         
-
-        $form['text'] = new Zend_Form_Element_Textarea('text');
-        $form['text']->setLabel('Text')
-            ->setRequired()    
-            ->setValue($this->data['text'])
-            ->setAttribs(array('rows'=>'3' , 'cols'=> '20'));
-        
         $descriptionType = $model->getCodeArray('DescriptionType', null, '1' , true);
         $form['type'] = new Zend_Form_Element_Select('type');
         $form['type']->setLabel('Description Type')  
@@ -25,13 +18,6 @@ class Iati_Aidstream_Form_Activity_Description extends Iati_Core_BaseForm
             ->setAttrib('class' , 'form-select')
             ->setMultioptions($descriptionType);
             
-        $lang = $model->getCodeArray('Language', null, '1' , true);
-        $form['xml_lang'] = new Zend_Form_Element_Select('xml_lang');
-        $form['xml_lang']->setLabel('Language')
-            ->setValue($this->data['@xml_lang'])
-            ->setAttrib('class' , 'form-select')
-            ->setMultioptions($lang);
-
         $this->addElements($form);
         return $this;
     }
