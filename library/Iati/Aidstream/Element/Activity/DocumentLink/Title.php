38c88388ffecd193885cf75cd7c70094858f7481
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-16 09:39:09 +0545

    Add narrative child element for iati-activity/document-link/title.

--- library/Iati/Aidstream/Element/Activity/DocumentLink/Title.php
+++ library/Iati/Aidstream/Element/Activity/DocumentLink/Title.php
@@ -3,10 +3,10 @@
 class Iati_Aidstream_Element_Activity_DocumentLink_Title extends Iati_Core_BaseElement
 {  
     protected $isMultiple = true;
+    protected $isRequired = true;
     protected $className = 'Title';
     protected $displayName = 'Title';
-    protected $isRequired = true;
-    protected $attribs = array('id' , '@xml_lang' , 'text');
-    protected $iatiAttribs = array('@xml_lang' , 'text');
     protected $tableName = 'iati_document_link/title';
+    protected $attribs = array('id');
+    protected $childElements = array('Narrative');
 }
