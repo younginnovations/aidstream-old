c5eb8af293b128f217e0c5dda0b8eefb829f3fb4
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-16 13:40:33 +0545

    Add narrative child element for iati-activity/transaction/description.

--- library/Iati/Aidstream/Element/Activity/Transaction/Description.php
+++ library/Iati/Aidstream/Element/Activity/Transaction/Description.php
@@ -1,10 +1,10 @@
 <?php
-
 class Iati_Aidstream_Element_Activity_Transaction_Description extends Iati_Core_BaseElement
 {
+	protected $isRequired = true;
     protected $className = 'Description';
     protected $displayName = 'Description';
-    protected $attribs = array('id', 'text', '@xml_lang');
-    protected $iatiAttribs = array('text', '@xml_lang');
     protected $tableName = 'iati_transaction/description';
+    protected $attribs = array('id');
+    protected $childElements = array('Narrative');  
 }
