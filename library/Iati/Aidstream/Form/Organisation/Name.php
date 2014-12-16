19875c39123fc1309591e43ef9dd121c9fd78ec8
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-16 15:36:17 +0545

    Add narrative child element for iati-organisation/name.

--- library/Iati/Aidstream/Form/Organisation/Name.php
+++ library/Iati/Aidstream/Form/Organisation/Name.php
@@ -10,17 +10,6 @@ class Iati_Aidstream_Form_Organisation_Name extends Iati_Core_BaseForm
         $form['id'] = new Zend_Form_Element_Hidden('id');
         $form['id']->setValue($this->data['id']);
             
-        $form['text'] = new Zend_Form_Element_Text('text');
-        $form['text']->setLabel('Name')
-            ->setValue($this->data['text'])
-            ->setAttribs(array('class' => 'form-text'));
-            
-        $lang = $model->getCodeArray('Language', null, '1' , true);
-        $form['xml_lang'] = new Zend_Form_Element_Select('xml_lang');
-        $form['xml_lang']->setLabel('Language')
-            ->setValue($this->data['@xml_lang'])
-            ->setAttrib('class' , 'form-select')
-            ->addMultioptions($lang);
 
         $this->addElements($form);
         return $this;
