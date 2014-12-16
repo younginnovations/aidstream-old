653bf59dcca0a0bd4f86b12d74c45535858731a8
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-16 14:03:42 +0545

    Add narrative child element for iati-activity/transaction/recipient-region.

--- library/Iati/Aidstream/Element/Activity/Transaction/RecipientRegion.php
+++ library/Iati/Aidstream/Element/Activity/Transaction/RecipientRegion.php
@@ -4,7 +4,8 @@ class Iati_Aidstream_Element_Activity_Transaction_RecipientRegion extends Iati_C
 {
     protected $className = 'RecipientRegion';
     protected $displayName = 'Recipient Region';
+    protected $tableName = 'iati_transaction/recipient_region';
     protected $attribs = array('id' , '@code', '@vocabulary');
     protected $iatiAttribs = array('@code', '@vocabulary');
-    protected $tableName = 'iati_transaction/recipient_region';
-}
+    protected $childElements = array('Narrative');
+    }
