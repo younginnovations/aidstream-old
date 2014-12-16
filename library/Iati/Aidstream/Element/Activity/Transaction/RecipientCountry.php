a471ae667f4c701c01f2fd58f46ac25d56a9bea4
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-16 13:56:00 +0545

    Add narrative child element for iati-activity/transaction/recipient-country.

--- library/Iati/Aidstream/Element/Activity/Transaction/RecipientCountry.php
+++ library/Iati/Aidstream/Element/Activity/Transaction/RecipientCountry.php
@@ -4,7 +4,9 @@ class Iati_Aidstream_Element_Activity_Transaction_RecipientCountry extends Iati_
 {
     protected $className = 'RecipientCountry';
     protected $displayName = 'Recipient Country';
+    protected $tableName = 'iati_transaction/recipient_country';
     protected $attribs = array('id' , '@code');
     protected $iatiAttribs = array('@code');
-    protected $tableName = 'iati_transaction/recipient_country';
+    protected $childElements = array('Narrative');
+    
 }
