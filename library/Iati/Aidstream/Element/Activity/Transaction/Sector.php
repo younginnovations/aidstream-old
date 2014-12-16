6ccdab547b39520a4303a9bf65d7aa3c547ccb57
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-16 13:54:50 +0545

    Add narrative child element for iati-activity/transaction/sector.

--- library/Iati/Aidstream/Element/Activity/Transaction/Sector.php
+++ library/Iati/Aidstream/Element/Activity/Transaction/Sector.php
@@ -5,7 +5,8 @@ class Iati_Aidstream_Element_Activity_Transaction_Sector extends Iati_Core_BaseE
     protected $isMultiple = true;
     protected $className = 'Sector';
     protected $displayName = 'Sector';
+    protected $tableName = 'iati_transaction/sector';
     protected $attribs = array('id' , '@vocabulary', '@code');
     protected $iatiAttribs = array('@vocabulary', '@code');
-    protected $tableName = 'iati_transaction/sector';
+    protected $childElements = array('Narrative');
 }
