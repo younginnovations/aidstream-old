fa674992593a30f8f9cba7377fabdf7844630192
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-16 10:57:33 +0545

    Add narrative child element for iati-activity/result/indicator/title.

---
+++ library/Iati/Aidstream/Element/Activity/Result/Indicator/Title/Narrative.php
@@ -0,0 +1,13 @@
+<?php
+
+class Iati_Aidstream_Element_Activity_Result_Indicator_Title_Narrative extends Iati_Core_BaseElement
+{
+    protected $isMultiple = true;
+    protected $className = 'Narrative';
+    protected $displayName = 'Narrative';
+    protected $tableName = 'iati_result/indicator/title/narrative';
+    protected $attribs = array('id', '@xml_lang', 'text');
+    protected $iatiAttribs = array('@xml_lang', 'text');
+    protected $viewScriptEnabled = true;
+
+}
