af2e0b446ff90441642a4e03e4e964ed012b6ece
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-18 14:33:52 +0545

    View update for organisation name.

--- library/Iati/Aidstream/Element/Organisation/Name.php
+++ library/Iati/Aidstream/Element/Organisation/Name.php
@@ -2,7 +2,6 @@
 
 class Iati_Aidstream_Element_Organisation_Name extends Iati_Core_BaseElement
 {
-    protected $isMultiple = true;
     protected $className = 'Name';
     protected $displayName = 'Name';
     protected $tableName = 'iati_organisation/name';
