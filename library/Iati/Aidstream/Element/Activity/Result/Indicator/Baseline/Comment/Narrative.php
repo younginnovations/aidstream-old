5c334826aec0fb50c1fd30c790cb648fffc158bc
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-19 10:46:32 +0545

    View update for activity result.

--- library/Iati/Aidstream/Element/Activity/Result/Indicator/Baseline/Comment/Narrative.php
+++ library/Iati/Aidstream/Element/Activity/Result/Indicator/Baseline/Comment/Narrative.php
@@ -3,7 +3,6 @@
 class Iati_Aidstream_Element_Activity_Result_Indicator_Baseline_Comment_Narrative extends Iati_Core_BaseElement
 {
     protected $isMultiple = true;
-    protected $isRequired = true;
     protected $className = 'Narrative';
     protected $displayName = 'Narrative';
     protected $tableName = 'iati_result/indicator/baseline/comment/narrative';
