5c0c1039002b1b45851b93afbd0e1f14bf79d6c2
Ramesh KC <ramesh.kc@yipl.com.np>
2014-12-23 11:20:48 +0545

    Update contact info.

--- library/Iati/Aidstream/Element/Activity/ActivityStatus.php
+++ library/Iati/Aidstream/Element/Activity/ActivityStatus.php
@@ -2,6 +2,7 @@
 
 class Iati_Aidstream_Element_Activity_ActivityStatus extends Iati_Core_BaseElement
 {
+    protected $isRequired = true;
     protected $className = 'ActivityStatus';
     protected $displayName = 'Activity Status';
     protected $tableName = 'iati_activity_status';
--- library/Iati/Aidstream/Form/Activity/ActivityStatus.php
+++ library/Iati/Aidstream/Form/Activity/ActivityStatus.php
@@ -13,7 +13,8 @@ class Iati_Aidstream_Form_Activity_ActivityStatus extends Iati_Core_BaseForm
         
         $activityStatus = $model->getCodeArray('ActivityStatus', null, '1' , true);
         $form['code'] = new Zend_Form_Element_Select('code');
-        $form['code']->setLabel('Activity Status')  
+        $form['code']->setLabel('Activity Status')
+            ->setRequired()
             ->setValue($this->data['@code'])
             ->setAttrib('class' , 'form-select')
             ->setMultioptions($activityStatus);
