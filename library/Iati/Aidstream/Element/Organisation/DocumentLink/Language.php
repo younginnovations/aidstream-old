3d596cdd9c67ba53eef5159b34c81b7f8484fba8
Saugat Acharya <mesaugat@gmail.com>
2014-12-25 09:36:38 +0545

    Override documentlink generateElementXml for organisation.

--- library/Iati/Aidstream/Element/Organisation/DocumentLink/Language.php
+++ library/Iati/Aidstream/Element/Organisation/DocumentLink/Language.php
@@ -8,5 +8,24 @@ class Iati_Aidstream_Element_Organisation_DocumentLink_Language extends Iati_Cor
     protected $attribs = array('id', '@code');
     protected $iatiAttribs = array('@code');
     protected $tableName = 'iati_organisation/document_link/language';
-    
+
+    protected function generateElementXml($elementData , $parent)
+    {
+        $eleName = $this->getXmlName();
+        $data = $this->getElementsIatiData($elementData);
+
+        if(!$this->hasData($data) && empty($this->childElements)) return;  //Donot generate xml if no iati data and no child
+        
+        // Get language value from code
+        if ($data['@code']) {
+            $data['@code'] = Iati_Core_Codelist::getCodeByAttrib($this->className , '@xml_lang' , $data['@code']);
+        } else {
+            return;
+        }
+
+        $xmlObj = $parent->addChild($eleName);
+        $xmlObj = $xmlObj->addAttribute('code', $data['@code']);
+        
+        return $xmlObj;
+    }
 }
