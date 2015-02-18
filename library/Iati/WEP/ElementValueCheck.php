c04c2bc70ce34f5c8bbff95f1f2665b14f099737
Saugat Acharya <mesaugat@gmail.com>
2015-02-18 11:46:26 +0545

    Mandatory fields check before activity status change to completed.

---
+++ library/Iati/WEP/ElementValueCheck.php
@@ -0,0 +1,100 @@
+<?php
+
+class Iati_WEP_ElementValueCheck {
+
+    protected static $requiredElements = array(
+        'ReportingOrg'     => 'Reporting Organisation',
+        'IatiIdentifier'   => 'Iati Identifier',
+        'Title'            => 'Title',
+        'Description'      => 'Description',
+        'ParticipatingOrg' => 'Participating Organisation',
+        'ActivityStatus'   => 'Activity Status',
+        'ActivityDate'     => 'Activity Date',
+        'Sector'           => 'Sector'
+    );
+
+    protected static $requiredElementValues = array(
+        'ActivityDate'     => '@iso_date',
+        'ParticipatingOrg' => '@role'
+    );
+
+    /**
+     * Check if default elements are present for an Activity and return errors if any.
+     * 
+     * @param array $activityIds
+     * @return array $errors
+     */
+    public static function checkDefaults($activityIds)
+    {
+        $errors = array();
+        foreach ($activityIds as $activityId) {
+            foreach (self::$requiredElements as $element => $fullName) {
+                $row = self::getRowSet($element, $activityId);
+                if (!$row['value']) {
+                    $errors[$activityId][] = $fullName;
+                }
+            }
+        }
+        
+        return $errors;
+    }
+
+    /**
+     * Check If Value Exist For An Element Of An Activity
+     * 
+     * @param type $name, className
+     * @param type $id, parentId
+     * @return array  
+     */
+    public static function getRowSet($name , $id)
+    {
+        $string = "Iati_Aidstream_Element_Activity_" . $name;
+        $obj = new $string;
+        $rowSet = $obj->fetchData($id , true);
+        if (!$rowSet) {
+            $content = false;
+            $value = false;
+        } else {
+            $content = true;
+            $value = true;
+            if (array_key_exists($name, self::$requiredElementValues)) {
+                if ($name == 'ParticipatingOrg') {
+                    if ($rowSet[0]['@role'] != 1 && $rowSet[0]['@role'] != 4) {
+                        $value = false;
+                    }
+                } elseif ($name == 'ActivityDate') {
+                    if (!$rowSet[0]['@iso_date'])
+                        $value = false;
+                }
+            }
+        }
+
+        return array(
+                'content' => $content,
+                'value'   => $value
+            );
+    }
+
+    /**
+     * Check if group elements are selected or not.
+     * 
+     * @param  [array]  $fieldGroup
+     * @param  [array]  $elements
+     * @return boolean
+     */
+    public static function hasGroupSet($fieldGroup, $elements)
+    {
+        $selected = false; 
+        foreach ($elements as $element) 
+        {
+            $name = strtolower(preg_replace('/([^A-Z_])([A-Z])/', '$1_$2', $element));
+            if ($fieldGroup[$name]) {   
+                $selected = true;
+                break;
+            }   
+        }
+        return $selected;
+    }
+
+}
+?>
