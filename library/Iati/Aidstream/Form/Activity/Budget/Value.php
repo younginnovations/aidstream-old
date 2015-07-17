b92fa4ca0409828c1c4989621142ab80390e7d44
kritinemkul <nemkulk@gmail.com>
2015-07-17 11:39:17 +0545

    fix show and hide default in activity - budget and planned disbursement

--- library/Iati/Aidstream/Form/Activity/Budget/Value.php
+++ library/Iati/Aidstream/Form/Activity/Budget/Value.php
@@ -22,18 +22,18 @@ class Iati_Aidstream_Form_Activity_Budget_Value extends Iati_Core_BaseForm
             ->addValidator(new App_Validate_NumericValue())
             ->setAttribs(array('class' => 'currency form-text')) ;
 
+        $form['value_date'] = new Zend_Form_Element_Text('value_date');
+        $form['value_date']->setLabel('Value Date')
+            ->setValue($this->data['@value_date'])
+            ->setRequired()
+            ->setAttrib('class' , 'datepicker' );
+
         $currency = $model->getCodeArray('Currency', null, '1' , true);
         $form['currency'] = new Zend_Form_Element_Select('currency');
         $form['currency']->setLabel('Currency')
             ->setValue($this->data['@currency'])
             ->setAttrib('class' , 'form-select')
             ->setMultioptions($currency);
-
-        $form['value_date'] = new Zend_Form_Element_Text('value_date');
-        $form['value_date']->setLabel('Value Date')
-            ->setValue($this->data['@value_date'])
-            ->setRequired()
-            ->setAttrib('class' , 'datepicker' );
 
         $this->addElements($form);
         return $this;
--- library/Iati/Aidstream/Form/Activity/PlannedDisbursement/Value.php
+++ library/Iati/Aidstream/Form/Activity/PlannedDisbursement/Value.php
@@ -21,15 +21,6 @@ class Iati_Aidstream_Form_Activity_PlannedDisbursement_Value extends Iati_Core_B
             ->setValue($this->data['@value_date'])
             ->setRequired()
             ->setAttrib('class' , 'datepicker' );
-            
-
-        $currency = $model->getCodeArray('Currency', null, '1' , true);
-        $form['currency'] = new Zend_Form_Element_Select('currency');
-        $form['currency']->setLabel('Currency')
-            ->setRequired()
-            ->setValue($this->data['@currency'])
-            ->setAttrib('class' , 'form-select')
-            ->setMultioptions($currency);
 
         $form['text'] = new Zend_Form_Element_Text('text');
         $form['text']->setLabel('Amount')
@@ -37,7 +28,12 @@ class Iati_Aidstream_Form_Activity_PlannedDisbursement_Value extends Iati_Core_B
            ->setValue($this->data['text'])
            ->setAttrib('class','form-text');
     
-
+        $currency = $model->getCodeArray('Currency', null, '1' , true);
+        $form['currency'] = new Zend_Form_Element_Select('currency');
+        $form['currency']->setLabel('Currency')
+            ->setValue($this->data['@currency'])
+            ->setAttrib('class' , 'form-select')
+            ->setMultioptions($currency);
 
         $this->addElements($form);
         return $this;
