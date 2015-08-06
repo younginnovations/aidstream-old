1316f7660289b0b23e998e9605ec4e2f805aa29a
kritinemkul <nemkulk@gmail.com>
2015-08-06 13:34:15 +0545

    update sector vocaublary and code

--- library/Iati/Aidstream/Form/Activity/Sector.php
+++ library/Iati/Aidstream/Form/Activity/Sector.php
@@ -11,7 +11,7 @@ class Iati_Aidstream_Form_Activity_Sector extends Iati_Core_BaseForm
         $form['id'] = new Zend_Form_Element_Hidden('id');
         $form['id']->setValue($this->data['id']); 
 
-        $vocabulary = $model->getCodeArray('Vocabulary', null, '1' , true);
+        $vocabulary = $model->getCodeArray('SectorVocabulary', null, '1' , true);
         $form['vocabulary'] = new Zend_Form_Element_Select('vocabulary');
         $form['vocabulary']->setLabel('Vocabulary')  
             ->setValue($this->data['@vocabulary'])   
