4b423f091a69d07bfd5e80284f6883ba779580d0
kritinemkul <nemkulk@gmail.com>
2015-08-21 15:28:52 +0545

    enter the old activity identifier in othe identifier only if the activity state is publish

--- application/modules/default/models/Wep.php
+++ application/modules/default/models/Wep.php
@@ -378,10 +378,17 @@ class Model_Wep extends Zend_Db_Table_Abstract
         $activityArray = $this->listAll('iati_activity', 'activities_id', $activitiesId);
         foreach ($activityArray as $key=>$activity) {
             $activityRow = $this->getRowById('iati_identifier', 'activity_id', $activity['id']);
-            $data['@ref'] = $activityRow['text'];
-            $data['@type'] = 5 ;
-            $data['activity_id'] = $activity['id'];
-            $this->insertRowsToTable('iati_other_identifier', $data);
+            if($activity['status_id'] == 4){
+                $data['@ref'] = $activityRow['text'];
+                $data['@type'] = 5 ;
+                $data['activity_id'] = $activity['id'];
+                $this->insertRowsToTable('iati_other_identifier', $data);
+            }
+            else{
+                $iatiIdentifier = $reportingOrgRef . '-' . $activityRow['activity_identifier']; 
+                $data1['text'] = $iatiIdentifier;
+                $this->updateRow('iati_identifier', $data1, 'activity_id', $activity['id']);
+            }
         }
         // For Organisation Data
         $organisation = $this->getRowById('iati_organisation', 'account_id', $identity->account_id);
