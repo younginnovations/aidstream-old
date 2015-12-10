b13f96787c5d8d4de3485c027a7725bfb64495ff
kritika.karmacharya <kritika.karmacharya@yipl.com.np>
2015-12-10 16:40:49 +0545

    changed budget-line value element required false

--- library/Iati/Aidstream/Element/Organisation/TotalBudget/BudgetLine/Value.php
+++ library/Iati/Aidstream/Element/Organisation/TotalBudget/BudgetLine/Value.php
@@ -2,8 +2,7 @@
 
 class Iati_Aidstream_Element_Organisation_TotalBudget_BudgetLine_Value extends Iati_Core_BaseElement
 {
-	protected $isRequired = true;
-    protected $className = 'Value';
+	protected $className = 'Value';
     protected $displayName = 'Value';
     protected $tableName = 'iati_organisation/total_budget/budget_line/value';
     protected $attribs = array('id' , '@currency', '@value_date','text');
